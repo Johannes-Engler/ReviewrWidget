7100443ce704088ced2fe54113d6c08724a645cb
Johannes Engler <engler.johannes@posteo.de>
2021-09-17 13:35:49 +0200

    Initial Commit

---
+++ ReviwerWidget/ReviwerWidget.h
@@ -0,0 +1,18 @@
+//
+//  ReviwerWidget.h
+//  ReviwerWidget
+//
+//  Created by Johannes Engler on 17.09.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReviwerWidget.
+FOUNDATION_EXPORT double ReviwerWidgetVersionNumber;
+
+//! Project version string for ReviwerWidget.
+FOUNDATION_EXPORT const unsigned char ReviwerWidgetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReviwerWidget/PublicHeader.h>
+
+
